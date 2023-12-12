0c36b7b6045d4d3a0ae0ed27a0e916bc7b50934e
nando008 <josefdorobles0801@gmail.com>
2023-12-12 17:08:08 -0500

    primero

---
+++ EjercicioTwo/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  
+  <div class="bg-slate-400 mx-auto h-[600px] w-[900px] mt-32 flex justify-center">
+    h
+    <div>
+
+    </div>
+
+    <div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+    </div>
+
+    <div>
+
+    </div>
+
+  </div>
+
+</template>
+
+
